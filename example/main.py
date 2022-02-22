6bb3c74e41b077ee15a9438ca0fbf573d0cbc481
BaySiyah <can@ksknsy.de>
2022-02-22 20:52:11 +0100

    updated example

--- example/main.py
+++ example/main.py
@@ -10,17 +10,17 @@ from data_access import DbConfiguration, DbConnector, RepositoryA, RepositoryB, 
 
 def configure_ioc() -> None:
     config = DbConfiguration("localhost", "admin", "", "test_db")
-    ioc.container.register_singleton(DbConfiguration, config)
-    ioc.container.register_per_request(DbConnector, name="MYSQL")
-    ioc.container.register_per_request(RepositoryA)
-    ioc.container.register_per_request(RepositoryB)
-    ioc.container.register_per_request(RepositoryC)
-    ioc.container.register_per_request(Service)
+    ioc.register_singleton(DbConfiguration, config)
+    ioc.register(DbConnector, name="MYSQL")
+    ioc.register(RepositoryA)
+    ioc.register(RepositoryB)
+    ioc.register(RepositoryC)
+    ioc.register(Service)
 
 
 def main() -> None:
     configure_ioc()
-    service = ioc.get_instance(Service)
+    service = ioc.get(Service)
     service.merge()
 
 
